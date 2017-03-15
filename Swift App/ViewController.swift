8012bfe538a27e5b87ffcef053ac6e5ae9fe0894
Trent Hand <livefree1983@gmail.com>
2017-03-15 12:28:33 -0500

    Added basic addition/subtraction functionality

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -9,40 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var theLabel: UILabel!
     
-    var tapCount = 0
+    @IBOutlet weak var text1: UITextField!
+    
+    @IBOutlet weak var text2: UITextField!
+    
     
     @IBAction func buttonTapped(_ sender: AnyObject) {
-       theLabel.text = "Hello there!"
-        tapCount += 1
-        print(tapCount)
-        if tapCount >= 20 {
-            theLabel.text = "You've tapped the button 20 times"
+        let addition = false
+        if addition {
+            theLabel.text = "The answer is \(Double(text1.text!)! + Double(text2.text!)!) "
         }
-        
+        else {
+            theLabel.text = "The answer is \(Double(text1.text!)! - Double(text2.text!)!) "
+            
+        }
     }
-
-//this is a comment.  I can comment a line by pressing the cmd+/ option
- /*This
- is
- a
- multiline
- comment
- */
-
+    
+    
+    
+    //this is a comment.  I can comment a line by pressing the cmd+/ option
+    /*This
+     is
+     a
+     multiline
+     comment
+     */
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         theLabel.text = "Trent!"
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
